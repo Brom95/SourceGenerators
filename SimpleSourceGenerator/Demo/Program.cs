2593930da68290a9bddea464fb53174af7d90297
Brom95 <wasalev@yandex.ru>
2023-04-12 21:12:20 +0300

    Init

---
+++ SimpleSourceGenerator/Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo;
+
+partial class Program
+{
+
+     static void Main(string[] args)
+    {
+        Console.WriteLine("!!!");
+        HelloFrom("it-irokez.ru");
+    }
+    static partial void HelloFrom(string name);
+}
