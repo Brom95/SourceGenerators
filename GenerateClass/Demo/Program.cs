14b1c2ab257647405cc8f8f879f99a6a8ccacd73
Brom95 <wasalev@yandex.ru>
2023-04-22 07:46:13 +0300

    class generator go brrrr!

---
+++ GenerateClass/Demo/Program.cs
@@ -0,0 +1,1 @@
+﻿GeneratedNamespace.GeneratedClass.GeneratedMethod();
---
+++ GenerateClass/Generator/ClassGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace Generator
+{
+    [Generator]
+    public class ClassGenerator:ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("GeneratedClass.cs", SourceText.From(@"
+namespace GeneratedNamespace
+{
+    public class GeneratedClass
+    {
+        public static void GeneratedMethod()
+        {
+            Console.WriteLine(""Irokez go brrrrr! "");
+        }
+    }
+}", Encoding.UTF8));
+        }
+    }
+}
