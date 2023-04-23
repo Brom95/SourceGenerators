67b86363b2adb2f77d032f98b2f5ba09756f6774
Brom95 <wasalev@yandex.ru>
2023-04-23 19:18:12 +0300

    Additional files compiller

---
+++ AdditionalFileTransformationGenerator/Demo/Program.cs
@@ -0,0 +1,1 @@
+﻿TotalPrint.MegaPrintClass.GeneratedMethod();
---
+++ AdditionalFileTransformationGenerator/Generator/Generator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Generator
+{
+    [Generator]
+    public class AddGenerator : ISourceGenerator
+    {
+
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sb = new StringBuilder();
+            var myFiles = context.AdditionalFiles.Where(f=>f.Path.EndsWith(".txt"));
+            foreach (var file in myFiles)
+            {
+                var content = File.ReadAllLines(file.Path);
+                foreach(var line in content)
+                {
+                    sb.AppendLine($"Console.WriteLine(@\"{line.Trim()}\");");
+                }
+            }
+           
+            context.AddSource($"MegaPrint.generated.cs", SourceText.From($@"
+namespace TotalPrint
+{{
+    public static class MegaPrintClass
+    {{
+
+        public static void GeneratedMethod()
+        {{
+            {sb.ToString()}
+        }}
+    }}
+}}", Encoding.UTF8));
+
+        }
+    }
+}
